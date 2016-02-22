a13218214c39e8fb46af88262e4ccbd86a2b9bd5
yoshida0261 <yoshida261@gmail.com>
2016-02-22 03:46:42 +0000

    digits

--- va.kt
+++ va.kt
@@ -1,8 +1,22 @@
 
 fun main(args : Array<String>){
   //Greeter(args[0]).greet()
-  
+  val digits = 123
+  val longInt = 123L
+  val hex =  0x0f
+  val bin = 0b00001011
+  val dbl = 123.5
+  val withe = 123.5e10
+  val flt = 123.5f  
   val num: Short = 3
   println(num);
+  println(digits);
+  println(longInt);
+  println(hex);
+  println(bin);
+  println(dbl);
+  println(withe);
+  println(flt);
+
 }
 
