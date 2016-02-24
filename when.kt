d02a744937208f3a16d1eca318302b51aceac1ca
yoshida0261 <yoshida261@gmail.com>
2016-02-24 03:42:57 +0000

    when

---
+++ when.kt
@@ -0,0 +1,14 @@
+
+fun main(args :Array<String>){
+  
+  val x = 1
+  when(x){
+    1->println("x==1")
+    2->print("x==2")
+    else ->{
+      print("x is neither 1 nor 2")
+    }
+  }
+  
+  
+}
