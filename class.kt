868af5c4c72f20176806ed11906c320f816a6c6f
yoshida0261 <yoshida261@gmail.com>
2016-02-25 03:57:51 +0000

    add property

--- class.kt
+++ class.kt
@@ -4,6 +4,15 @@ class Greeter(val name : String){
   }
 }
 
+class Person(val firstName : String, val lastName: String, var age: Int){
+  
+
+}
+
 fun main(args : Array<String>){
   Greeter(args[0]).greet()
+  val person = Person("joe", "smith", 30)
+  println(person.firstName)
+  person.age++
+  println(person.age)
 }
